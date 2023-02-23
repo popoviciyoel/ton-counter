f77637d237ba0241cdc2813ba7042160fe8cada5
Yoel Popovici <popoviciyoel@gmail.com>
2023-02-23 12:46:04 +0200

    my first ton web client app

---
+++ src/hooks/useCounterContract.ts
@@ -0,0 +1,44 @@
+import { Address, OpenedContract } from "ton-core";
+import { useEffect, useState } from "react";
+
+import Counter from "../contracts/counter";
+import { useAsyncInitialize } from "./useAsyncInitialize";
+import { useTonClient } from "./useTonClient";
+import { useTonConnect } from "./useTonConnect";
+
+export function useCounterContract() {
+  const client = useTonClient();
+  const [val, setVal] = useState<null | string>();
+  const { sender } = useTonConnect();
+
+  const sleep = (time: number) =>
+    new Promise((resolve) => setTimeout(resolve, time));
+
+  const counterContract = useAsyncInitialize(async () => {
+    if (!client) return;
+    const contract = new Counter(
+      Address.parse("EQD6qXLZBVY2aDK-tdLlDt9zG_6_UhYY_mAZtvRZnFVVLlA2") // replace with your address from tutorial 2 step 8
+    );
+    return client.open(contract) as OpenedContract<Counter>;
+  }, [client]);
+
+  useEffect(() => {
+    async function getValue() {
+      if (!counterContract) return;
+      setVal(null);
+      const val = await counterContract.getCounter();
+      setVal(val.toString());
+      await sleep(5000); // sleep 5 seconds and poll value again
+      getValue();
+    }
+    getValue();
+  }, [counterContract]);
+
+  return {
+    value: val,
+    address: counterContract?.address.toString(),
+    sendIncrement: () => {
+      return counterContract?.sendIncrement(sender);
+    },
+  };
+}
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+
+// this manifest is used temporarily for development purposes
+const manifestUrl =
+  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>
+);
